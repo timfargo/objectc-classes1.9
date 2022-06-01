ef2c9560c441baf91ffc1bdb41d13d1b3a95dd32
timfargo <isalintimur981@gmail.com>
2022-06-01 10:21:21 +0300

    объекты и методы 1.9

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Астрид", "Линдгрен");
+        Author author2 = new Author("Астрид", "Линдгрен");
+
+        Book book1 = new Book("Малыш и Карлосон", author1,1955);
+        Book book2 = new Book("Карлосон,который живёт на крыше", author2,1962);
+
+        System.out.println(book1.getTitle() + "" + book1.getYear() + "" + book1.getAuthor().getName() + "" + book1.getAuthor().getSurname());
+        System.out.println(book2.getTitle() + "" + book2.getYear() + "" + book2.getAuthor().getName() + "" + book2.getAuthor().getSurname());
     }
 }
